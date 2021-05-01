54d0c1c1c4fd6becb5e43c627e5c0dabe0e337fe
Daniela Máčalová <Dan.Macalova@gmail.com>
2021-05-01 12:24:50 +0200

    Objednávka

--- src/index.js
+++ src/index.js
@@ -15,3 +15,20 @@ navBtn.addEventListener('click', showHideNav);
 navAElm.forEach((elm) => {
   elm.addEventListener('click', showHideNav);
 });
+
+let ordered = false;
+
+const orderBtnElm = document.querySelector('.order-btn');
+orderBtnElm.addEventListener('click', () => {
+  if (ordered === false) {
+    document.querySelector('.drink__cup').classList.add('drink__cup--selected');
+    ordered = true;
+    orderBtnElm.textContent = 'Zrušit';
+  } else {
+    document
+      .querySelector('.drink__cup')
+      .classList.remove('drink__cup--selected');
+    orderBtnElm.textContent = 'Objednat';
+    ordered = false;
+  }
+});
