1dbcb226d28de9544c5cbceebe2611c602c95c52
Daniela Máčalová <Dan.Macalova@gmail.com>
2021-05-01 11:56:19 +0200

    Funkční hamburger

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
 
-console.log('funguju!');
+// console.log('funguju!');
+
+const navElm = document.querySelector('nav');
+const navAElm = document.querySelectorAll('nav a');
+
+const showHideNav = () => {
+  navElm.classList.toggle('nav-closed');
+};
+
+const navBtn = document.querySelector('#nav-btn');
+navBtn.addEventListener('click', showHideNav);
+
+navAElm.forEach((elm) => {
+  elm.addEventListener('click', showHideNav);
+});
